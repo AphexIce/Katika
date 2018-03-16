7942b5446723072eb22eb2b082af70d2993905e5
Arktix <lord_viking@msn.com>
2018-03-16 14:29:50 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+export const facebook = {
+  clientID: '1782018762106400',
+  clientSecret: 'd27b0314ef2e8788015cb570afdfbb9f',
+  callbackURL: 'http://localhost:3000/auth/facebook/callback',
+  profileFields: ['id', 'name', 'displayName', 'picture', 'email'],
+};
